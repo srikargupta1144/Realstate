51f82916246cecc9336dd0fc4dcb05e198f3d7c9
srikargupta1144 <srikartadakamalla22@gmail.com>
2024-02-11 00:19:17 +0530

    Connection to database

--- api/index.js
+++ api/index.js
@@ -1,8 +1,26 @@
-import express from 'express';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const mongoURI = process.env.MONGO;
+const port = process.env.PORT || 4000;
+
+mongoose.connect(mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+})
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB:", err);
+  });
 
 const app = express();
 
-
-app.listen(4000, () => {
-  console.log('Server is running on port number 4000!!!');
+app.listen(port, () => {
+  console.log(`Server is running on port number ${port}`);
 });
